91f2b6ffe5099f2a7bf896e2a969a16f75158838
Egor Kravchenko <egor.kravchenko@peanut.trade>
2024-11-08 17:07:27 +0200

    feat: add two string types

--- server/src/config/mongo/mongo.config.ts
+++ server/src/config/mongo/mongo.config.ts
@@ -3,7 +3,10 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 export default (): { mongoConfig: MongooseModuleOptions } => {
   return {
     mongoConfig: {
-      uri: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`,
+      uri:
+        process.env.IS_LOCAL === 'yes'
+          ? `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`
+          : `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`,
       autoIndex: true,
       autoCreate: true,
       retryDelay: 2000,
